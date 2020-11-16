0207a0d6bec3a0d269b239fb4ac960408565295f
deniska-sosiska <denchikarabik@gmail.com>
2020-11-16 17:10:44 +0200

    Prepairing

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>hello</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+@font-face {
+  font-family: 'Comfortaa';
+  src:url('./fonts/Comfortaa-Regular.ttf');
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Comfortaa";
 }
 </style>
