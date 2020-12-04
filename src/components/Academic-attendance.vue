88024a860072278be54676ebd600ab8ab660b50f
deniska <denchikarabik@gmail.com>
2020-12-04 18:45:22 +0200

    problem with checkbox

--- src/components/Academic-attendance.vue
+++ src/components/Academic-attendance.vue
@@ -1,21 +1,44 @@
 <template>
   <div class="table">
+    <div class="time" style="padding-top: 10px;">
+      <p>Дата: {{getNowDate}}</p>
+      <p>День неділі: {{getNowDay}}</p>
+      <p>Поточний час: {{getCurrentTime}}</p>
+    </div>
     <table>
       <thead>
+          <!-- <tr class="tr"><th style="border: 0px;" colspan="2"> </th>
+          <th>1 Пара</th><th>2 Пара</th><th>3 Пара</th><th>4 Пара</th> ✓-->
+        <!-- </tr> -->
         <tr><th>№</th><th>ПІБ</th>
-        <th>{{getCurrentLesson(0)}}</th>
-        <th>{{getCurrentLesson(1)}}</th>
-        <th>{{getCurrentLesson(2)}}</th>
-        <th>{{getCurrentLesson(3)}}</th>
+          <th>{{getCurrentLesson(1)}}</th>
+          <th>{{getCurrentLesson(2)}}</th>
+          <th>{{getCurrentLesson(3)}}</th>
+          <th>{{getCurrentLesson(4)}}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(student, index) in getDataOfCurrentGroup.students" :key="index">
           <td>{{++index}}</td><td>{{student.name}}</td>
-          <td class="para">✓</td>
-          <td class="para">✓</td>
-          <td class="para">✓</td>
-          <td class="para">✓</td>
+          <td class="para" @click="click(0)" v-if="qwe[0]">✓</td>
+          <td class="para" @click="click(0)" v-else> </td>
+
+          <td class="para" @click="click(1)" v-if="qwe[1]">✓</td>
+          <td class="para" @click="click(1)" v-else> </td>
+
+          <td class="para" @click="click(2)" v-if="qwe[2]">✓</td>
+          <td class="para" @click="click(2)" v-else> </td>
+
+          <td class="para" @click="qwe[3] = !qwe[3]" v-if="qwe[3]">✓</td>
+          <td class="para" @click="qwe[3] = !qwe[3]" v-else> </td>
+
+
+
+          <!-- <td class="para" @click="click(0)">{{qwe2(0)}}</td>
+          <td class="para" @click="click(1)">{{qwe2(1)}}</td>
+          <td class="para" @click="click(2)">{{qwe2(2)}}</td>
+          <td class="para" @click="click(3)">{{qwe2(3)}}</td> -->
+
         </tr>
       </tbody>
     </table>
@@ -30,40 +53,48 @@
     data () {
       return {
         dayNow: '',
-        nameDays: [
-          "Неділя",
-          "Понеділок",
-          "Вівторок",
-          "Середа",
-          "Четвер",
-          "friday",
-          "Субота",
-        ],
-        currentPara: [
-          "1","2","3","4"
-        ]
+        nameDays: ["Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"],
+        months: ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],
+        qwe: [true, false, false, false]
       }
     },
     computed: {
-      getDataOfCurrentGroup(){
-        return this.$store.getters.getDataOfCurrentGroup
-      }
+      getCurrentTime() {  
+        let hours = new Date().getHours(); let min = new Date().getMinutes()
+        hours < 10 ? hours = '0'+ hours : hours; min < 10 ? min = '0'+ min : min 
+        return hours + ':' +  min
+      },
+      getNowDay() {  return this.nameDays[new Date().getDay()]  },
+      getNowDate() {  return new Date().getDate() + '.' + new Date().getMonth() + '.' + new Date().getFullYear()  },
+      getDataOfCurrentGroup(){  return this.$store.getters.getDataOfCurrentGroup  }
     },
     mounted: function() {
-      // this.dayNow = new Date().getDate() + '.' + new Date().getMonth() + '.' + new Date().getFullYear()
-      // console.log(this.dayNow)
       let box = {"specialty": this.specialty, "group": this.group}
       this.$store.dispatch('updateDataOfCurrentGroup', box)
     },
     methods: {
+      qwe2(index) {
+        if (this.qwe[index]) {
+          console.log(this.qwe[index])
+          return "✓"
+        }
+        else {
+          console.log("false")
+          return " "
+        }
+      },
+      click(index) {
+        this.qwe[index] = !this.qwe[index]
+        console.log(`index: ${index}`, this.qwe[index])
+      },
       getCurrentLesson(index) {
         if (new Date().getDay() == 0 || new Date().getDay() == 6)
           return "Сьогодні вихідний"
         else {
           let currentDay = this.nameDays[new Date().getDay()]
-          let currentLesson = this.currentPara[index]
-          let answer = this.getDataOfCurrentGroup.schedule[currentDay][currentLesson]
-          if (answer == "Вікно") {}
+          let answer = this.getDataOfCurrentGroup.schedule[currentDay][index]
+          if (answer == "Вікно") 
+            return answer
           else 
             return answer
         } 
@@ -79,14 +110,18 @@
 <style scoped>
   table {
     margin: 0px auto;
-    margin-top: 30px;
-    padding: 40px;
+    margin-top: 10px;
+    padding: 20px 40px;
     border: 1px solid #ccc;
     border-radius: 25px;
   }
   th {
     padding: 0px 10px;
-    padding-bottom: 20px;
+    padding-bottom: 15px;
+  }
+  .tr > th {
+    padding-bottom: 0px;
+    border-bottom: solid 1px #ccc;
   }
   td {  
     border-bottom: solid 1px #ccc;
@@ -95,5 +130,9 @@
   .para {
     padding: 0xp 0px;
     text-align: center;
+  }
+  .time {
+    display: flex;
+    justify-content: space-around;
   }
 </style>
