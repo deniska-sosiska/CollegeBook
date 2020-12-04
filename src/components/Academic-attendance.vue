1f18f873fb856d9d8ebd1faddb670d30583dd5ec
deniska <denchikarabik@gmail.com>
2020-12-04 19:58:54 +0200

    made checkbox and hover

--- src/components/Academic-attendance.vue
+++ src/components/Academic-attendance.vue
@@ -2,7 +2,7 @@
   <div class="table">
     <div class="time" style="padding-top: 10px;">
       <p>Дата: {{getNowDate}}</p>
-      <p>День неділі: {{getNowDay}}</p>
+      <p>День тижня: {{getNowDay}}</p>
       <p>Поточний час: {{getCurrentTime}}</p>
     </div>
     <table>
@@ -10,35 +10,18 @@
           <!-- <tr class="tr"><th style="border: 0px;" colspan="2"> </th>
           <th>1 Пара</th><th>2 Пара</th><th>3 Пара</th><th>4 Пара</th> ✓-->
         <!-- </tr> -->
-        <tr><th>№</th><th>ПІБ</th>
-          <th>{{getCurrentLesson(1)}}</th>
-          <th>{{getCurrentLesson(2)}}</th>
-          <th>{{getCurrentLesson(3)}}</th>
-          <th>{{getCurrentLesson(4)}}</th>
+        <tr class="firstTR"><th>№</th><th>ПІБ</th>
+        <th v-for="(i, index) in 4" :key="index">{{getCurrentLesson(i)}}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(student, index) in getDataOfCurrentGroup.students" :key="index">
           <td>{{++index}}</td><td>{{student.name}}</td>
-          <td class="para" @click="click(0)" v-if="qwe[0]">✓</td>
-          <td class="para" @click="click(0)" v-else> </td>
 
-          <td class="para" @click="click(1)" v-if="qwe[1]">✓</td>
-          <td class="para" @click="click(1)" v-else> </td>
-
-          <td class="para" @click="click(2)" v-if="qwe[2]">✓</td>
-          <td class="para" @click="click(2)" v-else> </td>
-
-          <td class="para" @click="qwe[3] = !qwe[3]" v-if="qwe[3]">✓</td>
-          <td class="para" @click="qwe[3] = !qwe[3]" v-else> </td>
-
-
-
-          <!-- <td class="para" @click="click(0)">{{qwe2(0)}}</td>
+          <td class="para" @click="click(0)">{{qwe2(0)}}</td>
           <td class="para" @click="click(1)">{{qwe2(1)}}</td>
           <td class="para" @click="click(2)">{{qwe2(2)}}</td>
-          <td class="para" @click="click(3)">{{qwe2(3)}}</td> -->
-
+          <td class="para" @click="click(3)">{{qwe2(3)}}</td>
         </tr>
       </tbody>
     </table>
@@ -52,7 +35,6 @@
   export default {
     data () {
       return {
-        dayNow: '',
         nameDays: ["Неділя","Понеділок","Вівторок","Середа","Четвер","П'ятниця","Субота"],
         months: ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],
         qwe: [true, false, false, false]
@@ -84,7 +66,7 @@
         }
       },
       click(index) {
-        this.qwe[index] = !this.qwe[index]
+        Vue.set(this.qwe, index, !this.qwe[index])
         console.log(`index: ${index}`, this.qwe[index])
       },
       getCurrentLesson(index) {
@@ -93,9 +75,8 @@
         else {
           let currentDay = this.nameDays[new Date().getDay()]
           let answer = this.getDataOfCurrentGroup.schedule[currentDay][index]
-          if (answer == "Вікно") 
-            return answer
-          else 
+          // if (answer == "Вікно") {}
+          // else
             return answer
         } 
       }
@@ -114,8 +95,10 @@
     padding: 20px 40px;
     border: 1px solid #ccc;
     border-radius: 25px;
+    border-spacing: 0px;
   }
   th {
+    font-size: 18px;
     padding: 0px 10px;
     padding-bottom: 15px;
   }
@@ -125,14 +108,24 @@
   }
   td {  
     border-bottom: solid 1px #ccc;
-    padding: 0 20px 5px 20px;
+    padding: 10px 20px 4px 20px;
   }
   .para {
-    padding: 0xp 0px;
     text-align: center;
+    font-size: 12px;
+  }
+  .para:hover {
+    background: #ccefff;
+    cursor: pointer
+  }
+  tr:hover {
+    background: #e6f7ff;
   }
   .time {
     display: flex;
     justify-content: space-around;
+  }
+  .firstTR:hover {
+    background: #fff;
   }
 </style>
